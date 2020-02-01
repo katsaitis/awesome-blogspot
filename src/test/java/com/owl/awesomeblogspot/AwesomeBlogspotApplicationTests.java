35f05a9464094f72990ce02c13b7745b94ae369b
Christopher K <the4thplayer@Hotmail.com>
2020-02-01 23:27:48 +0200

    ...and tests

---
+++ src/test/java/com/owl/awesomeblogspot/AwesomeBlogspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.awesomeblogspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeBlogspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
