c2021a4d9d8a5d09790505b74d53895243bf9321
MariaKakaroni <prmousikos@yahoo.gr>
2020-02-21 18:57:38 +0200

    test

---
+++ src/main/resources/static/js/awesome_blogspot.js
@@ -0,0 +1,36 @@
+$(function(){
+
+    $("#communicationButton").click(function(){
+      $(this).hide();
+    });
+
+    
+    $("#postButton").click(function(){
+
+    });
+
+    $("#deleteButton").click(function(){
+
+    });
+
+    $("#editButton").click(function(){
+
+    });
+
+    $("#repostButton").click(function(){
+
+    });
+
+    $("#editButton").click(function(){
+
+    });
+
+    $("#loginButton").click(function(){
+
+    });
+
+    $("#loginButton").click(function(){
+
+    });
+    
+  });
