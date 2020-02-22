b5bfeba014fc054f378167eee7c3f3a9582872fc
Christopher K <the4thplayer@Hotmail.com>
2020-02-22 22:02:02 +0200

    functionality for creating posts

--- src/main/java/com/owl/awesomeblogspot/controller/AdminController.java
+++ src/main/java/com/owl/awesomeblogspot/controller/AdminController.java
@@ -1,20 +1,29 @@
 package com.owl.awesomeblogspot.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.owl.awesomeblogspot.service.PostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final PostService _postService;
+    private final ObjectMapper _mapper;
     private static final Logger LOGGER = LoggerFactory.getLogger(AdminController.class);
+
+    public AdminController(@Autowired PostService postService, @Autowired ObjectMapper objectMapper) {
+        this._postService = postService;
+        this._mapper = objectMapper;
+    }
 
     @GetMapping("/login")
     public String authenticate() {
@@ -24,6 +33,13 @@ public class AdminController {
     @GetMapping("/index")
     public String getAdminIndex() {
         return "admin";
+    }
+
+    @PostMapping("/post")
+    @ResponseBody
+    public String createPost(@RequestParam(value = "title") String title, @RequestParam(value = "body") String body) {
+        _postService.createPost(title, body);
+        return "Successfully created post";
     }
 
 }
--- src/main/java/com/owl/awesomeblogspot/controller/PostController.java
+++ src/main/java/com/owl/awesomeblogspot/controller/PostController.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
@@ -35,12 +33,5 @@ public class PostController {
     @GetMapping("/post/{id}")
     public Post getPost(@PathVariable Long id) {
         return _postService.findById(id);
-    }
-
-    @PostMapping("/post")
-    public String createPost(@RequestBody String post)
-    {
-        
-        return "Successfully created post";
     }
 }
