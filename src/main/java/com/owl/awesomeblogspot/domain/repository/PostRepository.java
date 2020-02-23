8aeaefa9f200fbe02b29244b8ebf4789e5dfbae8
christopher@tower <cazzaitis@live.com>
2020-02-23 19:56:02 +0200

    adding omitted post repo methods

--- src/main/java/com/owl/awesomeblogspot/domain/repository/PostRepository.java
+++ src/main/java/com/owl/awesomeblogspot/domain/repository/PostRepository.java
@@ -4,6 +4,13 @@ import com.owl.awesomeblogspot.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAll();
+
+    Optional<Post> findById(Long id);
 }
